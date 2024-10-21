7323f6622ff6c38bb7d29ac96b7e79c4979d3d6b
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-10-21 13:09:04 +0100

    WCC-6 - Replaced parameter validation with new class; also amended the output to be "{count} {filename}" as per challenge example

--- crickett-coding-challenges/wc-tool/src/main/java/com/wcl/wctool/WcTool.java
+++ crickett-coding-challenges/wc-tool/src/main/java/com/wcl/wctool/WcTool.java
@@ -1,22 +1,15 @@
 package com.wcl.wctool;
 
 import com.wcl.wctool.file.FileProcessor;
-
-import java.security.InvalidParameterException;
+import com.wcl.wctool.parameters.ParameterValidator;
 
 public class WcTool {
     public static void main(String... args) {
-        if (args.length == 0) {
-            throw new InvalidParameterException("No parameters were provided");
-        }
-
-        if (args.length > 1) {
-            throw new InvalidParameterException("The file name should be the only parameter");
-        }
-
+        final var parameterValidator = new ParameterValidator();
+        parameterValidator.validate(args);
 
         final var fileName = args[0];
         final var fileProcessor = new FileProcessor();
-        System.out.printf("Number of bytes %d%n",fileProcessor.numberOfBytes(fileName));
+        System.out.printf("%d %s%n",fileProcessor.numberOfBytes(fileName), fileName);
     }
 }
